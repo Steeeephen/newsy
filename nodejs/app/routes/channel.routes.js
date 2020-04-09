337fdf8eebd501f20287a231323490da3c8a4545
Stephen <stephenxcarroll@yahoo.co.uk>
2020-04-09 18:40:42 +0100

    Update channel.routes.js
    Added more endpoints

--- nodejs/app/routes/channel.routes.js
+++ nodejs/app/routes/channel.routes.js
@@ -1,16 +1,18 @@
 module.exports = (app) => {
     const channels = require('/app/controllers/channel.controllers.js');
 
-    // Create a new channel
+    // Create a new channel, fro editors
     app.post('/channel', channels.createNewChannel);
 
-    // get all channels
-    app.get('/channel/', channels.getAll);
+    // get all channel names, listed by name. We can use this for when an editor wishes to add a new channel
+    app.get('/channel/name', channels.getAll);
 
-    // get a channel of a certain id
-    app.get('/channel/:id', channels.getOne);
+    app.get('/channel/namesearch', channels.search);
 
-    // Update a channel of a certain id
-    app.put('/channel/:id', channels.updateChannel);
+    app.put('channel/name', channels.updateName);
+
+    app.put('channel/url', channels.updateURL);
+
+    app.put('channel/enable', channels.enableChannel);
 
 }
