afe8c0111d1262bce02d7d8bd6060db7fa84296f
Steeeephen <stephen.ofarrell64@gmail.com>
2020-04-09 22:47:35 +0100

    new user adds to mongodb

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
 from __future__ import print_function
 from flask import Flask, request, render_template, redirect
+from pymongo import MongoClient
 
 app = Flask(__name__, static_url_path='/static')
 
+client = MongoClient("mongodb+srv://stephen:l98waDrJSyCUspzw@newsy-98fzw.mongodb.net/test?retryWrites=true&w=majority")
+
+
+def new_user(form):
+	login = client['newsy']['login']
+	new_user_dict = {
+		"firstname":form['firstname'],
+		"lastname":form['lastname'],
+		"password":form['password'], #to be encrypted
+		"gender":form['gender'],
+		"email":form['email'],
+		"phone":form['phone'],
+		"question":form['question'],
+		"answer":form['answer'],
+		"editor":form['editor']=="True"
+	}
+	login.insert_one(new_user_dict)
 
 @app.route('/', methods = ['GET','POST'])
 @app.route('/register', methods = ['GET','POST'])
 def home():
 	if(request.method == "POST"):
-		print(request.form)
+		new_user(request.form) #need to verify fully
 		return 'main page goes here' #to add
 	else:
 		return render_template('index.html', title = "Newsy")
