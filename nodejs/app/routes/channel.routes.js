3c8e49898f349727bea4072e3d407ae286327227
Stephen <stephenxcarroll@yahoo.co.uk>
2020-04-23 18:20:54 +0100

    Updated routes for endpoints

--- nodejs/app/routes/channel.routes.js
+++ nodejs/app/routes/channel.routes.js
@@ -5,17 +5,20 @@ module.exports = (app) => {
     app.post('/channel', channels.createNewChannel);
 
     // get all channel names, listed by name. We can use this for when an editor wishes to add a new channel
-    app.get('/channel/name', channels.getAll);
+    app.get('/channel/names', channels.getAll);
+    
+    //return a single channel, with its name and url
+    app.get('/channel/:name', channels.getOne);
+    
+    //return a single channel url
+    app.get('/channel/:url', channels.display);
     
     // search for channel based on name, will return all names that contain a user regex
     app.get('/channel/namesearch', channels.search);
     
-    //update the channel name
-    app.put('channel/name', channels.updateName);
+    //update the channel according to name
+    app.put('channel/:name', channels.updateName);
     
-    //update the channel URL, if the RSS URL changes fro example 
-    app.put('channel/url', channels.updateURL);
-
     //toggle whether the channel is enabled or disabled according to a button click
     app.put('channel/enable', channels.toggleEnableChannel);
     
