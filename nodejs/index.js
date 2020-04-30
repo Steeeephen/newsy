f26b7b3311698024e2d592842d530081596c6dbb
Stephen <stephenxcarroll@yahoo.co.uk>
2020-04-30 22:54:48 +0100

    General updates and error handling
    Updated so that the port can be set by the app, but will run on 3000 otherwise.
    Added handling for Cross-origin resource sharing (CORS) errors
    Added a middleware for general error handling (404 and 500)

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
-var port = 3000;
+const port = process.env.PORT || 3000;
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+const cors = require('cors')
 //sets up database, pointing to file below
 var dbConfig = require('/config/database.config.js');
 
@@ -26,7 +27,25 @@ mongoose.connect(dbConfig.url, {
 });
 
 //sets up routes to the endpoints defined in file below
-var routes = require('app/routes/channel.routes.js')(app);
+var routes = require('/app/routes/channel.routes.js')(app);
+
+app.use(cors())
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}!`)
