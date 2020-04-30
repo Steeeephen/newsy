0ef9f0c3290829345ae93df4414fb65e54bd6f0c
Stephen <stephenxcarroll@yahoo.co.uk>
2020-04-30 22:41:02 +0100

    Finalised Controllers with error handling

--- nodejs/app/controllers/channel.controllers.js
+++ nodejs/app/controllers/channel.controllers.js
@@ -1,39 +1,98 @@
 var Channel = require('/app/models/channels.model.js');
+const mongoose = require('mongoose');
 
-//create channel according to details in request body (req.body)
+
+//create channel
 exports.createNewChannel = (req, res) => {
-  var new_Channel = new Channel(req.body);
-  new_Channel.save(function(err, channel){
-    if(err)
-      res.send(err);
-    res.send(channel);
+  var new_Channel = new Channel({
+    _id: new mongoose.Types.ObjectId(),
+    url: req.body.url,
+    name: req.body.name
   });
+  new_Channel.save()
+    .then(result => {
+      res.status(201).json({
+        message: "Created Feed successfully",
+        new_Channel: {
+          url: result.url,
+          name: result.name,
+          _id: result._id,
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 };
 
-//get single channel, display its name and url, accroding to req.params.name
 exports.getOne = (req, res) => {
-  Channel.find({name: req.params.name}, {name:1, url:1, _id:0} ,function(err, channel){
-    if(err)
-      res.send(err);
-    res.send(channel);
-  });
+  const id = req.params.channelId;
+  Channel.findById(id)
+    .select("name url _id")
+    .exec()
+    .then(doc => {
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res
+          .status(404)
+          .json({ message: "No valid entry found for provided ID" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({error: err});
+    });
 };
 
+
+//display the rss feed; this will get the url of the rss, which can then be parsed
 exports.display = (req, res) => {
-  Channel.find( {url: req.params.url},function(err, channel){
-    if(err)
-      res.send(err);
-    res.send(channel);
-  });
+  const id = req.params.channelId;
+  Channel.findById(id)
+    .select("name url _id")
+    .exec()
+    .then(doc => {
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res
+          .status(404)
+          .json({ message: "No valid entry found for provided ID" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({error: err});
+    });
 };
 
-//get all channels, displays just their name
+
+//get all channels
 exports.getAll = (req, res) => {
-  Channel.find({}, {name:1, _id:0} ,function(err, channel){
-    if(err)
-      res.send(err);
-    res.send(channel);
-  });
+  Channel.find({})
+    .select("name url _id")
+    .exec()
+    .then(docs => {
+      const response = {
+        count: docs.length,
+        feeds: docs.map(doc => {
+          return {
+            name: doc.name,
+            url: doc.url,
+            _id: doc._id,
+          };
+        })
+      };
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 };
 
 //search; get all channels matching a regex, 'var term', which will be whatever the editor types in
@@ -48,15 +107,22 @@ exports.search = (req, res) => {
 };
 
 // //updates the channel according to what is sent in the request params
-//NOT CURRENTLY WORKING, TO BE TESTED!
-// exports.update = (req, res) => {
-//   Channel.findOneAndUpdate({name: req.params.name},  {url: req.body.url}, {new: true},
-//   function(err, channel){
-//     if(err)
-//       res.send(err);
-//     res.send(channel);
-//   });
-// };
+exports.update = (req, res, next) => {
+  const id = req.params.channelId;
+  Channel.update({ _id: id }, req.body)//{name: req.params.name},  {url: req.body.url}, {new: true})
+  .exec()
+    .then(result => {
+      res.status(200).json({
+        message: "Channel updated",
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+};
 
 //enable/disable channel, var channelName will match whatever the channel name that is displayed in the HTML
 exports.toggleEnableChannel = (req, res) => {
@@ -74,10 +140,20 @@ exports.toggleEnableChannel = (req, res) => {
 
 //delete channel from database
 exports.deleteChannel = (req, res) => {
-  var channelName;
+  const id = req.params.channelId;
   //var channelName = document.getElementById("Title")
-  Channel.deleteOne({ name: channelName}, function(err, channel){
-    if(err)
-      res.send(err);
-  });
+  Channel.remove({ _id: id})
+  .exec()
+    .then(result => {
+      res.status(200).json({
+        message: "Channel deleted",
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 };
+
