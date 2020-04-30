43af6c7d16b577a5318ef080c8c969c6c78aea2d
Stephen <stephenxcarroll@yahoo.co.uk>
2020-04-30 22:44:08 +0100

    Removed display, changed otheres

--- nodejs/app/routes/channel.routes.js
+++ nodejs/app/routes/channel.routes.js
@@ -1,27 +1,21 @@
 module.exports = (app) => {
     const channels = require('/app/controllers/channel.controllers.js');
 
-    // Create a new channel, for editors
     app.post('/channel', channels.createNewChannel);
 
     // get all channel names, listed by name. We can use this for when an editor wishes to add a new channel
-    app.get('/channel/names', channels.getAll);
-    
-    //return a single channel, with its name and url
-    app.get('/channel/:name', channels.getOne);
-    
-    //return a single channel url
-    app.get('/channel/:url', channels.display);
-    
-    // search for channel based on name, will return all names that contain a user regex
+    app.get('/channel/', channels.getAll);
+
+    app.get('/channel/names', channels.getAllByName);
+
+    app.get('/channel/:channelId', channels.getOne);
+
     app.get('/channel/namesearch', channels.search);
-    
-    //update the channel according to name
-    app.put('channel/:name', channels.updateName);
-    
-    //toggle whether the channel is enabled or disabled according to a button click
-    app.put('channel/enable', channels.toggleEnableChannel);
-    
-    //Delete a selected channel from the database
-    app.put('channel/name', channels.deleteChannel);
+
+    app.patch('/channel/:channelId', channels.update);
+
+    app.put('channel/enabled', channels.toggleEnableChannel);
+
+    app.delete('/channel/:channelId', channels.deleteChannel);
+    // // get a channel of a certain id
 }
