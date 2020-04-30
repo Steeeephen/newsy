de524cc05b31af426dfb20a920ad2f5273dfac92
Stephen <stephenxcarroll@yahoo.co.uk>
2020-04-30 22:43:12 +0100

    Update channel.controllers.js

--- nodejs/app/controllers/channel.controllers.js
+++ nodejs/app/controllers/channel.controllers.js
@@ -48,27 +48,6 @@ exports.getOne = (req, res) => {
 };
 
 
-//display the rss feed; this will get the url of the rss, which can then be parsed
-exports.display = (req, res) => {
-  const id = req.params.channelId;
-  Channel.findById(id)
-    .select("name url _id")
-    .exec()
-    .then(doc => {
-      if (doc) {
-        res.status(200).json(doc);
-      } else {
-        res
-          .status(404)
-          .json({ message: "No valid entry found for provided ID" });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({error: err});
-    });
-};
-
-
 //get all channels
 exports.getAll = (req, res) => {
   Channel.find({})
