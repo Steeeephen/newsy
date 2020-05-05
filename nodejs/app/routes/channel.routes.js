431df218ad2b55d2d374a5093d8df047218987f1
Stephen <stephenxcarroll@yahoo.co.uk>
2020-05-05 16:47:36 +0100

    Removed redundant endpoints

--- nodejs/app/routes/channel.routes.js
+++ nodejs/app/routes/channel.routes.js
@@ -10,17 +10,8 @@ module.exports = (app) => {
     //get a channel by ID, as the url is in the form :channelID, the url for the channel with ID 4567819 would be '/channel/4567819'
     app.get('/channel/:channelId', channels.getOne);
 
-    //get a channel matching the url, will just display
-    app.get('/channel/:url', channels.display);
-
-    //returns all channels matching a regex term passed by the user, as long as the channel name contains that term, it will return
-    app.get('/channel/namesearch', channels.search);
-
     //updates the channel, is passed an id, updates its record in the database according to the request body.
     app.patch('/channel/:channelId', channels.update);
-
-    //enables or disables the channel (opposite of the current value of enabled) when a button is clicked
-    app.put('/channel/enabled', channels.toggleEnableChannel);
 
     //deletes the channel whose id is passed.
     app.delete('/channel/:channelId', channels.deleteChannel);
